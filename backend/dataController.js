b8ee38255a21d610e5a39609bc12271bc8756bec
davidrtlake <davidrtlake@gmail.com>
2025-02-27 23:18:53 +1000

    feat: Sending data to local MySQL database now functional. Also can now get entries. Now using pool connections. Updated packages.

--- backend/dataController.js
+++ backend/dataController.js
@@ -2,83 +2,85 @@ const mysql = require("mysql2");
 
 const writeToFile = false;
 
-let scrapedData = require("./data_3.json");
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_NAME:", process.env.DB_NAME);
 
-let con = mysql.createConnection({
+let scrapedData = require("./data/2025/data.json");
+
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10, // Max connections in pool
+  queueLimit: 0,
 });
 
 function sendEventsToDB() {
-  con.connect((err) => {
+  pool.connect((err) => {
     if (err) return console.error(err.message);
     console.log("Insert events into events table.");
     console.log("Connected to the MySQL server.");
     let eventCols = [
-      `event_id`,
-      `contact_email`,
-      `contact_name`,
-      `contact_phone`,
-      `description`,
       `end_date`,
-      `org_unit`,
-      `org_unit_url`,
+      `event_id`,
+      `period`,
       `start_date`,
-      `time`,
       `title`,
       `url`,
     ];
-    // First, execute the DELETE query on the events_tags table
-    con.query("DELETE FROM events_tags", (err, result) => {
-      if (err) throw err;
-      console.log("Events_tags table cleared.");
-    });
     // Execute the DELETE query on the events table
-    con.query("DELETE FROM events", (err, result) => {
+    pool.query("DELETE FROM events", (err, result) => {
       if (err) throw err;
       console.log("Events table cleared.");
     });
 
-    for (let entry of scrapedData) {
-      let sql = `INSERT INTO events (${eventCols.join(
-        ", "
-      )}) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
-      let [startDate, endDate, time] = convertDateAndTime(
-        entry["Date:"],
-        entry["Time:"]
-      );
-      let entryData = [
-        entry["id"],
-        entry["Email:"],
-        entry["Name:"],
-        entry["Phone:"],
-        entry["Full Description:"],
-        endDate,
-        entry["Org.Unit:"]["org_unit"],
-        entry["Org.Unit:"]["org_unit_url"],
-        startDate,
-        time,
-        entry["title"],
-        entry["URL:"],
-      ];
-      con.query(sql, entryData, (err, result) => {
-        if (err) throw err;
-        console.log(`Inserted rows ${entry["id"]}/${scrapedData.length}`);
-      });
+    let id = 1;
+    for (let year of scrapedData) {
+      for (let period of year["periods"]) {
+        for (let month of period["months"]) {
+          for (let day of month["days"]) {
+            let sql = `INSERT INTO events (${eventCols.join(
+              ", "
+            )}) VALUES (?, ?, ?, ?, ?, ?)`;
+            let [startDate, endDate] = convertDate(
+              day["date"],
+              month["name"],
+              year["year"]
+            );
+            let entryData = [
+              endDate,
+              id++,
+              period["name"],
+              startDate,
+              day["description"],
+              day["link"],
+            ];
+            console.log(`Inserted row ${id - 1}`);
+            pool.query(sql, entryData, (err, result) => {
+              if (err) throw err;
+            });
+          }
+        }
+      }
     }
-    connectTagsAndEvents();
-    con.end((err) => {
-      if (err) return console.error(err.message);
-      console.log("Closed the database connection.");
-    });
+  });
+}
+
+function getEntriesFromDB() {
+  pool.query("SELECT * FROM events", (err, result) => {
+    if (err) throw err;
+    console.log("Requesting data.");
+    console.log(result);
   });
 }
 
 function sendTagsToDB() {
-  con.connect((err) => {
+  pool.connect((err) => {
     if (err) return console.error(err.message);
 
     console.log("Connected to the MySQL server.");
@@ -87,12 +89,12 @@ function sendTagsToDB() {
       tags = {};
 
     // First, execute the DELETE query on the events_tags table
-    con.query("DELETE FROM events_tags", (err, result) => {
+    pool.query("DELETE FROM events_tags", (err, result) => {
       if (err) throw err;
       console.log("Events_tags table cleared.");
     });
     // Execute the DELETE query on the tags table
-    con.query("DELETE FROM tags", (err, result) => {
+    pool.query("DELETE FROM tags", (err, result) => {
       if (err) throw err;
       console.log("Tags table cleared.");
     });
@@ -106,13 +108,13 @@ function sendTagsToDB() {
       count++;
       let sql = `INSERT INTO tags (${eventCols.join(", ")}) VALUES (?, ?, ?)`;
       let entryData = [count, tag, tags[tag]];
-      con.query(sql, entryData, (err, result) => {
+      pool.query(sql, entryData, (err, result) => {
         if (err) throw err;
         console.log(`Inserted rows ${count}/${Object.keys(tags).length}`);
       });
     }
     connectTagsAndEvents();
-    con.end((err) => {
+    pool.end((err) => {
       if (err) return console.error(err.message);
       console.log("Closed the database connection.");
     });
@@ -120,25 +122,25 @@ function sendTagsToDB() {
 }
 
 function connectTagsAndEvents() {
-  con.connect((err) => {
+  pool.connect((err) => {
     if (err) return console.error(err.message);
 
     console.log("Connecting tags and events tables into events_tags table.");
     console.log("Connected to the MySQL server.");
     // First, execute the DELETE query on the events_tags table
-    con.query("DELETE FROM events_tags", (err, result) => {
+    pool.query("DELETE FROM events_tags", (err, result) => {
       if (err) throw err;
       console.log("Events_tags table cleared.");
     });
     for (let entry of scrapedData) {
       for (let tag of entry["Event category(s):"]) {
-        let tagTitle = con.escape(tag["tagsText"]); // Escape the tag title
-        let entryID = con.escape(entry["id"]);
+        let tagTitle = pool.escape(tag["tagsText"]); // Escape the tag title
+        let entryID = pool.escape(entry["id"]);
         let sql = `INSERT INTO events_tags (event_id, tag_id) 
                SELECT e.event_id, t.tag_id 
                FROM events e, tags t 
                WHERE e.event_id = ${entryID} AND t.title = ${tagTitle}`;
-        con.query(sql, (err, result) => {
+        pool.query(sql, (err, result) => {
           if (err) throw err;
           console.log(
             `Inserted rows for ${entryID}/${scrapedData.length} entries.`
@@ -146,73 +148,129 @@ function connectTagsAndEvents() {
         });
       }
     }
-    con.end((err) => {
+    pool.end((err) => {
       if (err) return console.error(err.message);
       console.log("Closed the database connection.");
     });
   });
 }
 
-function convertDateAndTime(date, time) {
+function convertDate(date, month, year) {
+  const dateLen = date.length;
+  // console.log("");
+  // console.log(date, date.length);
+
+  let monthNums = month.replace(/[A-Za-z\s]+/g, "");
+  if (monthNums.startsWith("20")) {
+    year = monthNums;
+  }
+  let startMonth = "";
+  let endMonth = "";
+
+  for (let l of date) {
+    if (startMonth.length < 3) {
+      startMonth += l.match(/[A-Za-z]/g) ? l : "";
+    } else {
+      endMonth += l.match(/[A-Za-z]/g) ? l : "";
+    }
+  }
+
+  if (endMonth.length === 0) {
+    endMonth = startMonth;
+  }
+
+  date = date.replace(/[A-Za-z\s]+/g, "").replace(/\u{2013}/gu, "-");
+  // console.log(date, date.length);
+
   let startDate = date.split("-")[0];
   let endDate = date.split("-")[1] || startDate;
-  startDate = startDate.split(",")[1].split(" ");
-  endDate = endDate.split(",")[1].split(" ");
-  startDate = `${startDate[2]}-${convertMonthToNumber(startDate[1])}-${
-    startDate[0]
-  }`;
-  endDate = `${endDate[2]}-${convertMonthToNumber(endDate[1])}-${endDate[0]}`;
-  if (time.toLowerCase() === "all day") {
-    time = "00:00";
-  } else {
-    time = time.slice(0, -3);
-    time = time.length < 5 ? "0" + time : time;
+  if (startDate.length === 1) {
+    startDate = "0".concat(startDate);
   }
-  return [startDate, endDate, time];
+  if (endDate.length === 1) {
+    endDate = "0".concat(endDate);
+  }
+
+  startMonth = convertMonthToNumber(startMonth);
+  endMonth = convertMonthToNumber(endMonth);
+
+  // console.log(
+  //   startDate,
+  //   endDate,
+  //   startMonth,
+  //   endMonth,
+  //   "month",
+  //   month,
+  //   "year",
+  //   year,
+  //   "monthNums",
+  //   monthNums
+  // );
+
+  if (dateLen > 6 && startDate === endDate) {
+    throw new Error("Start and end date should be different.");
+  } else if (dateLen <= 6 && startDate !== endDate) {
+    throw new Error("Start and end date should be the same.");
+  }
+  if (dateLen > 10 && startMonth === endMonth) {
+    throw new Error("Start and end month should be different.");
+  }
+
+  startDate = `${year}-${startMonth}-${startDate}`;
+  endDate = `${year}-${endMonth}-${endDate}`;
+  // console.log(startDate, endDate);
+
+  return [startDate, endDate];
 }
 
 function convertMonthToNumber(month) {
   switch (month) {
-    case "January":
+    case "Jan":
       return "01";
       break;
-    case "February":
+    case "Feb":
       return "02";
       break;
-    case "March":
+    case "Mar":
       return "03";
       break;
-    case "April":
+    case "Apr":
       return "04";
       break;
     case "May":
       return "05";
       break;
-    case "June":
+    case "Jun":
       return "06";
       break;
-    case "July":
+    case "Jul":
       return "07";
       break;
-    case "August":
+    case "Aug":
       return "08";
       break;
-    case "September":
+    case "Sep":
       return "09";
       break;
-    case "October":
+    case "Oct":
       return "10";
       break;
-    case "November":
+    case "Nov":
       return "11";
       break;
-    case "December":
+    case "Dec":
       return "12";
       break;
     default:
+      throw new Error("Invalid month");
       return month;
       break;
   }
 }
 
-module.exports = { sendEventsToDB, sendTagsToDB, connectTagsAndEvents };
+module.exports = {
+  sendEventsToDB,
+  getEntriesFromDB,
+  sendTagsToDB,
+  connectTagsAndEvents,
+};
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
 const browserObject = require("./browser");
 const scraperController = require("./pageController");
-// const dataController = require("./dataController");
+const dataController = require("./dataController");
 
 //Start the browser and create a browser instance
 let browserInstance = browserObject.startBrowser();
 
 // Pass the browser instance to the scraper controller
-scraperController(browserInstance);
+// scraperController(browserInstance);
 
 // dataController.connectTagsAndEvents();
-// dataController.sendTagsToDB();
+// dataController.sendEventsToDB();
+dataController.getEntriesFromDB();
